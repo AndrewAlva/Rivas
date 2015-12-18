74f7d9b82fc9f9e603ab0f6d72c4688adab97a79
Lucky Arte <vanhuten@Vanhuten.local>
2015-12-18 14:40:06 -0600

    Layout HTML/PHP

---
+++ header.php
@@ -0,0 +1,27 @@
+<header id="menuHeader">
+	<li class="easeInOut">
+		<a class="easeInOut" href="#">
+			Nosotros
+		</a>
+	</li>
+	<li class="easeInOut">
+		<a class="easeInOut" href="#">
+			Servicios
+		</a>
+	</li>
+	<li id="logoLi" class="easeInOut">
+		<a class="easeInOut" href="#">
+			<img src="https://placeholdit.imgix.net/~text?txtsize=12&txt=171%C3%9747&w=171&h=47" alt="Rivas & Fregoso">
+		</a>
+	</li>
+	<li class="easeInOut">
+		<a class="easeInOut" href="#">
+			Bolsa de Trabajo
+		</a>
+	</li>
+	<li class="easeInOut">
+		<a class="easeInOut" href="#">
+			Contacto
+		</a>
+	</li>
+</header>
---
+++ index.php
@@ -0,0 +1,146 @@
+<html class="no-js" lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        
+        <div id="fullWrapper">
+            <?php
+                include("header.php");
+            ?>
+
+            <div id="sectionsWrapper">
+                <div id="section1" class="rfSections">
+                    <div id="homeDescription">
+                        <h1 class="tdropShadow">Somos una firma mexicana de contadores públicos y consultores</h1>
+                        <h2 class="tdropShadow">Destacada por su liderazgo en la prestación de servicios integrales y personalizados a las empresas privadas y a sus propietarios en nuestro país.</h2>
+                    </div>
+
+                    <div id="homeServiceList">
+                        <li>
+                            <div class="sTitle">Derecho Laboral</div>
+                            <div class="sContent easeInOut">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis temporibus ullam adipisci explicabo quia sit harum eveniet, amet laboriosam. Corporis magnam ducimus  architecto aut porro!</div>
+                        </li>
+                        <li>
+                            <div class="sTitle">Derecho Laboral</div>
+                            <div class="sContent easeInOut">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint unde quos qui facere quas quibusdam laudantium, illum est, a, suscipit rem explicabo aliquid  harum dolores.</div>
+                        </li>
+                        <li>
+                            <div class="sTitle">Derecho Laboral</div>
+                            <div class="sContent easeInOut">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis recusandae dolor sapiente nobis sit, inventore veritatis nihil possimus vitae, saepe asperiores  Voluptas autem nemo placeat.</div>
+                        </li>
+                    </div>
+                </div>
+                
+
+                <div id="section2" class="rfSections">
+                    <div id="nosotrosWrap">
+                        <div class="halfColumn tdropShadow">
+                            <h3>Nosotros</h3>
+                            <p>Rivas y Fregoso fue fundada en Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ab eum voluptatibus velit dicta, nam ratione! Sit magnam repellat ut nisi iusto exercitationem, non unde pariatur quo corporis, quasi a!</p>
+                        </div>
+
+                        <div class="halfColumn tdropShadow">
+                            <h4>Misión</h4>
+                            <p>Rivas y Fregoso fue fundada en Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ab eum voluptatibus velit dicta, nam ratione! Sit magnam repellat ut nisi iusto exercitationem, non unde pariatur quo corporis, quasi a!</p>
+
+                            <h4>Visión</h4>
+                            <p>Rivas y Fregoso fue fundada en Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ab eum voluptatibus velit dicta, nam ratione! Sit magnam repellat ut nisi iusto exercitationem, non unde pariatur quo corporis, quasi a!</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div id="section3" class="rfSections">
+                    <div id="serviceWrap" class="tdropShadow">
+                        <h3>Servicios</h3>
+                        <div class="servicesList easeInOut showService">
+                            <h4>Contabilidad</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, aperiam laboriosam in unde placeat adipisci, temporibus delectus magnam, atque deleniti, facilis doloribus nesciunt repudiandae deserunt animi amet beatae perspiciatis nostrum!</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Administración de Bienes</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo est dolores, sapiente, voluptate fuga illum nesciunt neque officia? Laborum, natus, deserunt iste asperiores impedit quae voluptatum illum fugit quidem saepe!</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Impuestos</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, provident deserunt. Doloremque quia quibusdam ipsam ab debitis non possimus et dolorum facilis mollitia! Cupiditate iure molestias ipsam consequatur quis, dolore?</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Nóminas</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi accusantium repellat quia quis, nesciunt doloribus dicta. Distinctio neque molestias et ipsa temporibus magnam aliquam, quidem ipsam, quas eaque nostrum voluptatem.</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Seguridad</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas fugit expedita eius, minus debitis earum repudiandae. Facilis praesentium commodi harum dignissimos, incidunt, aliquam deserunt distinctio pariatur voluptatum illo nam nisi.</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Consultoría</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste suscipit fugiat possimus facilis quis temporibus et molestiae aut veritatis repellat nemo ducimus id pariatur assumenda, distinctio, inventore deserunt corporis provident.</p>
+                        </div>
+
+                        <div class="servicesList easeInOut">
+                            <h4>Legal</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In pariatur amet, commodi nisi excepturi provident natus beatae, sapiente odit quas illo quos explicabo molestias accusantium consectetur repudiandae recusandae, a obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div id="section4" class="rfSections">
+                    <div id="bolsaWrap">
+                        <h3>Bolsa de Trabajo</h3>
+                        <div class="innerBox">
+                            <div class="halfColumn"></div>
+                            <div class="halfColumn"></div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div id="section5" class="rfSections"></div>
+            </div>
+        </div>
+
+        
+
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
